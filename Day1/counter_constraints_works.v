57bc21848a1ae35cc77184073b126cb055f06893
nandithaec <nanditha.ec@gmail.com>
2022-06-01 18:49:07 +0530

    counter for constraints issue

---
+++ Day1/counter_constraints_works.v
@@ -0,0 +1,60 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 23.03.2022 00:47:56
+// Design Name: 
+// Module Name: counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module counter(clk,reset,count);
+input clk,reset;
+output reg [3:0] count = 4'b0000;
+reg [25:0] count_reg;
+reg clk_div = 1'b0;
+
+always @ (posedge clk)
+begin
+if (reset)
+    begin
+        clk_div <= 1'b0;
+        count_reg <= 26'd0;
+     end
+else
+    begin
+        count_reg <= count_reg + 1;
+        if (count_reg == 26'h3ffffff) // for synthesis
+    //   if (count_reg == 26'd12) // for simulation
+        begin
+            clk_div <= ~ clk_div;
+            count_reg <= 26'd0;
+        end
+    end
+ end
+  
+ always @ (posedge clk_div)
+ begin
+ if (reset)
+ begin
+    count <= 4'b0000;
+ end
+ else
+ begin
+    count <= count + 1;
+ end
+ end
+
+endmodule
